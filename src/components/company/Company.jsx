d0a4f9cae792740c52d21ba86070f85d6b3ba31c
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-08-17 14:33:08 +0600

    complete company section

--- src/components/company/Company.jsx
+++ src/components/company/Company.jsx
@@ -1,7 +1,16 @@
 
 const Company = () => {
   return (
-    <div>Company</div>
+    <section className="container mx-auto overflow-x-hidden xl:mt-16">
+      <div className="flex items-center space-x-32 company-animation">
+        <img src="/public/company/Company logo-0.png" alt="" />
+        <img src="/public/company/Company logo-1.png" alt="" />
+        <img src="/public/company/Company logo-2.png" alt="" />
+        <img src="/public/company/Company logo-3.png" alt="" />
+        <img src="/public/company/Company logo-4.png" alt="" />
+        <img src="/public/company/Company logo-5.png" alt="" />
+      </div>
+    </section>
   )
 }
 
